87697a966c6f3f524666bd05fdf9a96b2a834400
Divya123solanki <solankidivya817@gmail.com>
2024-09-29 17:15:39 +0530

    Add files via upload

---
+++ AreaofRectangle.java
@@ -0,0 +1,13 @@
+public class AreaofRectangle {
+    public static void main(String[] args) {
+        int a=96;
+        int b=8;
+        int l;
+        int p;
+        l=a/b;
+        System.out.println(l);
+        p=2*(l+b);
+        System.out.println(p);
+
+    }
+}
