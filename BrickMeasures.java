69a3c29d3460e6920301e94df1a51b2e475cad7c
Divya123solanki <solankidivya817@gmail.com>
2024-09-30 10:01:36 +0530

    Add files via upload

---
+++ BrickMeasures.java
@@ -0,0 +1,9 @@
+public class BrickMeasures {
+    public static void main(String[] args) {
+        double l1=15,b1=8,h1=5,l2=15,b2=10,h2=8,v1,v2,n;
+        v1=l1*b1*h1;
+        v2=l2*10*b2*10*h2*10;
+        n=v2/v1;
+        System.out.println(n);
+    }
+}
