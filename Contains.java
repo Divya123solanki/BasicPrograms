264494bcfe29c0b67d8839a45ba4a192f8ec4bdd
Divya123solanki <solankidivya817@gmail.com>
2024-10-01 17:07:31 +0530

    Add files via upload

---
+++ Contains.java
@@ -0,0 +1,15 @@
+public class Contains {
+    public static void main(String args[]){
+        int a[]={1,2,3,4,5};
+       
+        int n=2;
+        for(int i=0;i<a.length;i++){
+           
+                if(a[i]==n){
+                    System.out.print("value found");
+                }
+              
+            }
+        }
+    }
+
