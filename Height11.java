b8902ca6a34618dc6c0464114841b13554c48b14
Divya123solanki <solankidivya817@gmail.com>
2024-10-02 16:07:56 +0530

    Add files via upload

---
+++ Height11.java
@@ -0,0 +1,10 @@
+public class Height11 {
+    public static void main(String args[]){
+        double a=320,b=8,h=5,x,result;
+       x=(a*2)/b*h;
+      System.out.println(x);
+       
+     result=x*x;
+        System.out.println(result); 
+    }  
+}
