b211ad6c51c10ba56b6a55836c21cc0a6d771292
Divya123solanki <solankidivya817@gmail.com>
2024-10-02 16:07:02 +0530

    Add files via upload

---
+++ Garden15.java
@@ -0,0 +1,9 @@
+public class Garden15 {
+    public static void main(String[] args) {
+        int l=22,b=15,s=21,a,a1,d;
+        a=l*b;
+        a1=s*s;
+        d=a1-a;
+        System.out.println(d);
+    }
+}
