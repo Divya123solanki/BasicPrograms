36e66d180126b0bcc4c531292581874ed53b0511
Divya123solanki <solankidivya817@gmail.com>
2024-10-02 16:06:33 +0530

    Add files via upload

---
+++ Fence5.java
@@ -0,0 +1,9 @@
+public class Fence5 {
+    public static void main(String[] args) {
+        int c=1600,l=20,r=25,p,a,b;
+        p=c/r;
+       b= (p/2)-l;
+       a=l*b;
+       System.out.println(a);
+    }
+}
