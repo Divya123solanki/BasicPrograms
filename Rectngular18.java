c1d75b9a08bc6fd407fd6ce56a11aa6e71be0565
Divya123solanki <solankidivya817@gmail.com>
2024-10-04 15:27:20 +0530

    Add files via upload

---
+++ Rectngular18.java
@@ -0,0 +1,10 @@
+public class Rectngular18 {
+    public static void main(String args[]){
+        double l=50,b=30;
+        double d,p,result;
+        p=2*(l+b);
+        d=p*10;
+        result=d/1000;
+        System.out.println(result);
+    }
+}
