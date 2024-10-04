933e67424f95a70c5457e044cdb1de8517a8b50b
Divya123solanki <solankidivya817@gmail.com>
2024-10-04 15:26:39 +0530

    Add files via upload

---
+++ Perimeter.java
@@ -0,0 +1,14 @@
+public class Perimeter{
+    public static void main(String args[]){
+        int pr=230;
+        int lr=70;
+        int b;
+        int w=0;
+        int area;
+    w=(pr/2)-lr;
+        System.out.println(w);
+        area=lr*w;
+        System.out.println(area);
+
+    }
+}
