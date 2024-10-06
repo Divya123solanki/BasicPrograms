9a5c6a354e03ca874ed3a2de3552dab2ad623ee1
Divya123solanki <solankidivya817@gmail.com>
2024-10-06 13:44:41 +0530

    Add files via upload

---
+++ Tiles3.java
@@ -0,0 +1,12 @@
+public class Tiles3 {
+    public static void main(String[] args) {
+        int A1,l1=520,b1=140;
+        int A2,l2=13,b2=7;
+        int A3;
+        A1=l1*b1;
+        A2=l2*b2;
+        A3=A1/A2;
+        System.out.println(A3);
+         
+    }
+}
