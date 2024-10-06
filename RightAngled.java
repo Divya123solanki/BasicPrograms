0cea324e7371c717b070f796b79bd6e762f94afb
Divya123solanki <solankidivya817@gmail.com>
2024-10-06 13:43:45 +0530

    Add files via upload

---
+++ RightAngled.java
@@ -0,0 +1,10 @@
+public class RightAngled {
+    public static void main(String args[]){
+        double hy=13,h=12,b,t,a;
+        b=(hy*hy)-(h*h);
+        System.out.print(b);
+        double result=Math.sqrt(b);
+        a=1/2*result*h;
+        System.out.println(a);
+    }
+}
