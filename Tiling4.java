bdaa5328fce726c65ee410c9e43d4f7d1e8fe969
Divya123solanki <solankidivya817@gmail.com>
2024-10-07 17:16:48 +0530

    Add files via upload

---
+++ Tiling4.java
@@ -0,0 +1,10 @@
+public class Tiling4 {
+    public static void main(String args[]){
+        int l=300,w=150,a,t,result;
+        a=l*w;
+        t=a/100;
+        result=t*6;
+        System.out.println(result);
+
+    }
+}
