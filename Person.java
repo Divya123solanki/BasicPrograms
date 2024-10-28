18d50ce39f2a4334b155ba96aafff0308c3be430
Divya123solanki <solankidivya817@gmail.com>
2024-10-28 10:17:38 +0530

    Add files via upload

---
+++ Person.java
@@ -0,0 +1,29 @@
+//package New folder.Inheritance;
+
+ public class Person {
+    private String name;
+    private int age;
+    public person(String name, int age){
+        this.name=name;
+        this.age=age;
+    }
+    public void displayDetail(){
+        System.out.println("Name"+name);
+        System.out.println("Age"+age);
+    }
+}
+class Employee extends person{
+     double salary;
+     public Employee1(salary Double){
+        this.salary=salary;
+     }
+     public void displayDetails(){
+        System.out.println("salary"+salary);
+     }
+}
+public class Driver{
+    public static void main(String[] args) {
+        Employee e=new Employee(17000);
+        Person p=new
+    }
+}
